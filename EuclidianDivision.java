0025c94a5f7868192ec9d8be6e4a158089fc896f
Julius <jr1184404@swccd.edu>
2025-09-02 20:12:26 +0000

    Created EuclidianDivision.java

---
+++ EuclidianDivision.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class EuclidianDivision{
+    public static void main(String[] args){
+        int divisor;
+        Scanner input = new Scanner(System.in)
+        int num = input.nextInt(divisor);
+        
+        int dividend;
+        int num = input.nextInt(dividend);
+        System.out.println("Enter a divisor and dividend")
+       
+        
+        
+        int quotient;
+        int remainder;
+        
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter an integer");
+        int num = input.nextInt();
+        System.out.println("The number is" + num);
+    }
+}
